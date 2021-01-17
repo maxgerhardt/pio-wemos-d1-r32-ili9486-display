a2f51917519a428ee8ea8e3fcccd2670d002b9c0
unknown <maximilian.gerhardt@rub.de>
2021-01-17 13:50:30 +0100

    Add intial state

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+int i=0;
+
+void setup() {
+  tft.begin();
+  tft.setRotation(1);
+}
+
+void loop() {
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+//  tft.drawCentreString("Hello world " + String(i), 100, 80, 1);
+  tft.drawCircle(360/2, 420/2, 500, TFT_RED);
+}
